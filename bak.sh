69f1ff3b60368b0ef5d30e5b17d0c5b20707c14d
GAOYANG <2633018506@qq.com>
2018-10-26 11:46:54 +0800

    备份脚本

--- bak.sh
+++ bak.sh
@@ -9,5 +9,5 @@ BAK_DIR="/backup/$HOSTNAME-$IP"
 mkdir -p $BAK_DIR && \
 cd $DATA_DIR && \
 find ./ -type d|xargs tar zcf $BAK_DIR/${DATE}.tar.gz && \
-#需提前在备份端/backup目录下创建以hostname-ip命令的目录
+#需提前在备份端/backup目录下创建以hostname-ip命名的目录
 scp -p $BAK_DIR/${DATE}.tar.gz root@10.0.0.12:$BAK_DIR
